664d9b0460a6ae9dac48b3961b005f6713557065
Meghan Domeck <mdomeck@aol.com>
2020-09-28 16:21:44 -0400

    file setup

---
+++ src/main/java/com/mdomeck/songr/HomeController.java
@@ -0,0 +1,10 @@
+package com.mdomeck.songr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+
+    @Contoller public class HomeCOntroller {
+        @GetMapping("/")
+    }
+}
