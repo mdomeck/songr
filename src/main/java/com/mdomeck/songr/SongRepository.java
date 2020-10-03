a77f3442c8b1e7e6ea2c63184be65d51463badc0
Meghan Domeck <mdomeck@aol.com>
2020-10-02 21:10:35 -0400

    got it done

--- src/main/java/com/mdomeck/songr/SongRepository.java
+++ src/main/java/com/mdomeck/songr/SongRepository.java
@@ -1,8 +1,14 @@
 package com.mdomeck.songr;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
 
+//    @Query("SELECT * FROM song WHERE song.album_id = ?1")
+//    List<Song> findByAlbumId(long id);
 
 }
